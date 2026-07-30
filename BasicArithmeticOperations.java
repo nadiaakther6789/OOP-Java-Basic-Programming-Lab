d6bd47756cc98e06bb1ed6ce04455163faadae96
Nadia Akther <nadiaakther502@gmail.com>
2026-07-30 11:48:14 +0600

    Implement basic arithmetic operations in Java

---
+++ BasicArithmeticOperations.java
@@ -0,0 +1,19 @@
+public class BasicArithmeticOperations {
+    public static void main(String[] args){  
+
+      int first_number= 125;
+      int second_number=24;
+
+       int sum = first_number+second_number;
+       int subtract = first_number-second_number;
+       int multipication = first_number*second_number;
+       int division =  first_number/second_number;
+       int modulus =  first_number%second_number;
+
+       System.out.println("Sum:" + sum);
+       System.out.println("Subtract:" + subtract);
+       System.out.println("multipication:" + multipication);
+       System.out.println("division:" + division);
+       System.out.println("modulus:" + modulus);
+    }
+}
