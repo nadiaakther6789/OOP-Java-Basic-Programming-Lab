e7fdc868c4a99439ea67c355e36d9a79fd719b3f
Nadia Akther <nadiaakther502@gmail.com>
2026-07-30 20:27:17 +0600

    Update division.java

--- division.java
+++ division.java
@@ -1,7 +1,6 @@
 public class division {
     public static void main(String[] args0){
-        int a = 50;
-        int b = 3;
+        int a = 50, b = 3;
         int c = a/b;
         System.out.print("Division of two numbers:" + c); 
     }
