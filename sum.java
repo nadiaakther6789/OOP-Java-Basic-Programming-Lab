b931eed24f83841a7078723137f1191d9e57f94e
Nadia Akther <nadiaakther502@gmail.com>
2026-07-30 19:46:59 +0600

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,6 @@
 public class sum {
     public static void main(String[] args){
-        int a = 74;
-        int b = 36;
-        System.out.println("Sum:" + (a + b));
+        int a = 74,b = 36;
+        System.out.println("Sum = " + (a + b));
     }
 }
