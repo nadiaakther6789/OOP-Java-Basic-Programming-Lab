fa14e8459177728eba0305eb0ab343342f81f1de
Nadia Akther <nadiaakther502@gmail.com>
2026-07-30 20:21:19 +0600

    Update product.java

--- product.java
+++ product.java
@@ -3,6 +3,6 @@ public class product {
         int Input_first_number = 25;
         int Input_second_number = 5;
         int product = Input_first_number * Input_second_number;
-        System.out.println("The product of the two numbers is: " + product);
+        System.out.println("The product of the two numbers is = " + product);
     }
 }
