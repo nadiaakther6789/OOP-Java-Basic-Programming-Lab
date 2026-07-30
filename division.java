d735f19cf7a483efba17c9ff7ac207cabe09c380
Nadia Akther <nadiaakther502@gmail.com>
2026-07-30 11:43:37 +0600

    Create division.java

---
+++ division.java
@@ -0,0 +1,8 @@
+public class division {
+    public static void main(String[] args0){
+        int a = 50;
+        int b = 3;
+        int c = a/b;
+        System.out.print("Division of two numbers:" + c); 
+    }
+}
